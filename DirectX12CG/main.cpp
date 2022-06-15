82473c27af4490e31d6a16358c4a49c824e4cae8
楓 紅桜 <rngkq545@gmail.com>
2022-06-15 18:10:38 +0900

    aa

--- DirectX12CG/main.cpp
+++ DirectX12CG/main.cpp
@@ -73,8 +73,8 @@ using namespace MCB;
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,
     _In_ int nCmdShow)
 {
-    {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    {
     //_CrtSetBreakAlloc(1030);
 
     //int* hoge = new int(4);
