b03c407fdfe8d264b8164e1e93b883c931dc6eba
楓 紅桜 <rngkq545@gmail.com>
2022-06-15 09:09:48 +0900

    aaa

--- DirectX12CG/main.cpp
+++ DirectX12CG/main.cpp
@@ -134,6 +134,7 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
     Box.begin()->scale = {5,5,5};
    
+    int distance = 20;
 
 
     for (int i = 0; i < Box.size(); i++)
