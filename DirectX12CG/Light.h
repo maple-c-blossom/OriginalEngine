501fdef54664cedc4d61f2904560b94a57af8745
LC1A_03_アベ_リュウタ <k021g1118@g.neec.ac.jp>
2022-09-23 23:08:04 +0900

    とりあえず資料進めるための前準備(まだ終わってない部分あり)

--- DirectX12CG/Light.h
+++ DirectX12CG/Light.h
@@ -19,13 +19,21 @@ namespace MCB
 			Float3 lightColor;
 		};
 		Microsoft::WRL::ComPtr<ID3D12Resource> lightConstBuff;
-
+		inline void SetLightDir(Vector3D lightdir) 
+		{
+			lightdir.V3Norm();
+			this->lightdir = lightdir;
+		}
+		inline const Vector3D& GetLightDir() { return lightdir; }
+		inline void SetLightColor(const Float3& lightcolor) { this->lightColor = lightcolor; }
+		inline const Float3& GetLightColor() { return lightColor; }
+		inline void SetActive(bool active) { this->active = active; }
+		inline bool IsActive() { return active; }
 	private:
 
 		Vector3D lightdir = { 1,0,0 };
 		Float3 lightColor = { 1,1,1 };
-		bool dirty = false;
-
+		bool active = true;
 		//static DirLight* LightCreate();
 		//void SetLightDir(Vector3D lightdir);
 		//void SetLightColor(Float3 lightColor);
--- DirectX12CG/LightGroup.h
+++ DirectX12CG/LightGroup.h
@@ -1,11 +1,54 @@
 #pragma once
 #include "Light.h"
 #include "PointLight.h"
-
+#include <wrl.h>
 namespace MCB
 {
 	class LightGroup
 	{
+	public:
+		static const int DirLightNum = 3;
+
+		struct LightGroupConstBuff
+		{
+			Float3 ambientColor;
+			float pad1;
+			DirLight::LightConstBuffData dirLights[DirLightNum];
+		};
+	private:
+
+		/// <summary>
+/// 静的初期化
+/// </summary>
+/// <param name="device">デバイス</param>
+		static void StaticInitialize(ID3D12Device* device);
+
+		/// <summary>
+		/// インスタンス生成
+		/// </summary>
+		/// <returns>インスタンス</returns>
+		static LightGroup* Create();
+
+	private: // 静的メンバ変数
+		// デバイス
+		static ID3D12Device* device;
+
+	public: 
+		void Initialize();
+		void Update();
+		void Draw(ID3D12GraphicsCommandList* cmdList, UINT rootParameterIndex);
+		void TransferConstBuff();
+		void DefaultLightSet();
+		void SetAmbientColor(const Float3& color);
+		void SetDirLightIsActive(int lightNum, bool active);
+		void SetDirLightForLightDir(int lightindexNum, const Vector3D& lightdir);
+		void SetDirLightColor(int lightindexNum, const Float3& lightcolor);
+
+	private:
+		Microsoft::WRL::ComPtr<ID3D12Resource> constBuff;
+		Float3 ambientColor = { 1,1,1 };
+		DirLight dirLights[DirLightNum];
+		bool isUpdate = false;
 	};
 
 }
