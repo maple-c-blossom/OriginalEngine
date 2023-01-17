49130049e8aca34f6c81fb3b59905e02ddd40444
LC1A_03_アベ_リュウタ <k021g1118@g.neec.ac.jp>
2023-01-17 11:57:40 +0900

    error殺した

--- DirectX12CG/main.cpp
+++ DirectX12CG/main.cpp
@@ -31,15 +31,28 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
         debugController->SetEnableGPUBasedValidation(TRUE);
     }
 
-    //if (SUCCEEDED(Dx12::GetInstance()->device.Get()->QueryInterface(IID_PPV_ARGS(infoQueue.GetAddressOf()))))
-    //{
-    //    infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
-    //    infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
-    //}
+
+
 #endif
 #pragma endregion デバック時のみ
     //-------------
     Dx12::GetInitInstance();
+#ifdef _DEBUG
+    if (SUCCEEDED(Dx12::GetInstance()->device.Get()->QueryInterface(IID_PPV_ARGS(infoQueue.GetAddressOf()))))
+    {
+        infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
+        infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
+        D3D12_MESSAGE_ID denyids[] = { D3D12_MESSAGE_ID_RESOURCE_BARRIER_BEFORE_AFTER_MISMATCH, D3D12_MESSAGE_ID_GPU_BASED_VALIDATION_INCOMPATIBLE_RESOURCE_STATE };
+        D3D12_MESSAGE_SEVERITY severities[] = { D3D12_MESSAGE_SEVERITY_INFO };
+        D3D12_INFO_QUEUE_FILTER filter{};
+        filter.DenyList.NumIDs = _countof(denyids);
+        filter.DenyList.pIDList = denyids;
+        filter.DenyList.NumSeverities = _countof(severities);
+        filter.DenyList.pSeverityList = severities;
+        infoQueue->PushStorageFilter(&filter);
+
+    }
+#endif
     Input* input = Input::GetInitInstance();
     Depth depth;
     ShaderResource::GetInitInstance();
