2ff891d0a437eaeb500c6721aef2b6d280aab1db
LC1A_03_アベ_リュウタ <k021g1118@g.neec.ac.jp>
2023-01-20 13:53:50 +0900

    a

--- DirectX12CG/Collider.cpp
+++ DirectX12CG/Collider.cpp
@@ -5,9 +5,9 @@ using namespace MCB;
 bool MCB::CalcSphere(Sphere sphereA, Sphere sphereB)
 {
 
-	int hitX = (sphereA.centerPosition.x - sphereB.centerPosition.x) * (sphereA.centerPosition.x - sphereB.centerPosition.x);
-	int hitY = (sphereA.centerPosition.y - sphereB.centerPosition.y) * (sphereA.centerPosition.y - sphereB.centerPosition.y);
-	int hitZ = (sphereA.centerPosition.z - sphereB.centerPosition.z) * (sphereA.centerPosition.z - sphereB.centerPosition.z);
+	int hitX = (sphereA.centerPosition.vec.x - sphereB.centerPosition.vec.x) * (sphereA.centerPosition.vec.x - sphereB.centerPosition.vec.x);
+	int hitY = (sphereA.centerPosition.vec.y - sphereB.centerPosition.vec.y) * (sphereA.centerPosition.vec.y - sphereB.centerPosition.vec.y);
+	int hitZ = (sphereA.centerPosition.vec.z - sphereB.centerPosition.vec.z) * (sphereA.centerPosition.vec.z - sphereB.centerPosition.vec.z);
 	int hitR = (sphereB.radius + sphereA.radius) * (sphereB.radius + sphereA.radius);
 	hitX = Abs(hitX);
 	hitY = Abs(hitY);
@@ -20,9 +20,22 @@ bool MCB::CalcSphere(Sphere sphereA, Sphere sphereB)
 	return false;
 }
 
+bool MCB::CalcPlaneSpher(Plane plane, Sphere sphere)
+{
+	float dist = sphere.centerPosition.GetV3Dot(plane.normal);
+	dist = dist - plane.distance;
+	if (fabsf(dist) > sphere.radius) return false;
+	return true;
+}
+
+bool MCB::CalcTriangleSpher(Triangle plane, Sphere sphere)
+{
+	return false;
+}
+
 bool MCB::CalcRaySphere(Ray ray, Sphere sphere)
 {
-	Vector3D rayToSphere(ray.StartPosition, sphere.centerPosition);
+	Vector3D rayToSphere(ray.StartPosition, sphere.centerPosition.vec);
 	ray.rayVec.V3Norm();
 	float tempPos = rayToSphere.GetV3Dot(ray.rayVec);
 	ray.rayVec = ray.rayVec * tempPos;
@@ -37,7 +50,7 @@ bool MCB::CalcRaySphere(Ray ray, Sphere sphere)
 	}
 	Vector3D aVec;
 
-	aVec.V3Get(sphere.centerPosition, Temp);
+	aVec.V3Get(sphere.centerPosition.vec, Temp);
 
 	float a = aVec.V3Len();
 
