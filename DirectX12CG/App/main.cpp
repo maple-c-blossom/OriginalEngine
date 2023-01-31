7c47368ad04938b5a2647aa979a093bb89420900
LC1A_03_アベ_リュウタ <k021g1118@g.neec.ac.jp>
2023-01-31 13:05:08 +0900

    hogehoge

--- DirectX12CG/App/main.cpp
+++ DirectX12CG/App/main.cpp
@@ -30,9 +30,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
         debugController->EnableDebugLayer();
         debugController->SetEnableGPUBasedValidation(TRUE);
     }
-
-
-
 #endif
 #pragma endregion デバック時のみ
     //-------------
