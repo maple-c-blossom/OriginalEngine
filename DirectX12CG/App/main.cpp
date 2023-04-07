e23bcceb8cb20df302394303edf885d224b54b97
楓 紅桜 <rngkq545@gmail.com>
2023-04-07 10:52:49 +0900

    1行減らした(WinMainの引数の折り返しを消しただけ)

--- DirectX12CG/App/main.cpp
+++ DirectX12CG/App/main.cpp
@@ -1,6 +1,5 @@
 #include "App.h"
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,
-    _In_ int nCmdShow)
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,_In_ int nCmdShow)
 {
     std::make_unique<MCB::App>()->Run();
 	return 0;
