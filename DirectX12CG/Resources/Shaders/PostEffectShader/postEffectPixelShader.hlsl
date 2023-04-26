285ad288d6a6bc03d90e86de30c69f051e609dc9
LC1A_03_アベ_リュウタ <k021g1118@g.neec.ac.jp>
2023-04-26 23:53:47 +0900

    最低仕様終了

--- DirectX12CG/Resources/Shaders/PostEffectShader/postEffectPixelShader.hlsl
+++ DirectX12CG/Resources/Shaders/PostEffectShader/postEffectPixelShader.hlsl
@@ -6,12 +6,23 @@ SamplerState smp : register(s0);
 
 float4 main(VSOutput input) : SV_TARGET
 {
-    float4 texColor0 = tex0.Sample(smp, input.uv);
-    float4 texColor1 = tex1.Sample(smp, input.uv);
-    float4 finalColor = texColor0;
-    if(fmod(input.uv.y,0.1f) < 0.05f)
+    if (fmod(input.uv.y, 0.1f) < 0.05f)
     {
-        finalColor = texColor1;
+        float uChange = 1 / 1280.f;
+        float vChange = 1 / 720.f;
+        float4 texColor1sum = { 0, 0, 0, 0 };
+        float uForRange = 3;
+        float vForRange = 3;
+        for (float v = 0; v < vForRange; v++)
+        {
+            for (float u = 0; u < uForRange; u++)
+            {
+                float2 offSet = ((u - 1) * uChange, (v - 1) * vChange);
+                texColor1sum += tex1.Sample(smp, saturate(input.uv - offSet));
+            }
+        }
+        return texColor1sum / (uForRange * vForRange);
     }
-    return finalColor;
+    float4 texColor0 = float4(1 - tex0.Sample(smp, input.uv).rgb, 1);
+    return texColor0;
 }
