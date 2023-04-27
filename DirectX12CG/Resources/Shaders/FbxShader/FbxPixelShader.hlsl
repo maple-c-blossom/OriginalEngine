f538263db1ee176e8726fd06545f21e146a6e04e
楓 紅桜 <rngkq545@gmail.com>
2023-04-27 11:02:18 +0900

    警告消した

--- DirectX12CG/Resources/Shaders/FbxShader/FbxPixelShader.hlsl
+++ DirectX12CG/Resources/Shaders/FbxShader/FbxPixelShader.hlsl
@@ -3,9 +3,10 @@
 Texture2D<float4> tex : register(t0);
 SamplerState smp : register(s0);
 
-PSOutput main(GSOutput input)
+PSOutput toonShader(GSOutput input)
 {
     PSOutput output;
+    float2 threshold = { 0.475, 0.575 };
     float4 texcolor = float4(tex.Sample(smp, input.uv));
     float3 eyedir = normalize(cameraPos - input.worldpos.xyz);
     float3 ambient = m_ambient;
@@ -14,11 +15,12 @@ PSOutput main(GSOutput input)
     {
         if (dirLights[i].active)
         {
-            float3 dotlightnormal = saturate(dot(dirLights[i].lightv, input.normal));
+            float3 dotlightnormal = dot(dirLights[i].lightv, input.normal);
             float3 reflect = normalize(-dirLights[i].lightv + 2 * dotlightnormal * input.normal);
-            float3 diffuse = dotlightnormal * m_diffuse * texcolor.rgb * color.rgb;
+            float3 diffuse = saturate(dotlightnormal) * m_diffuse * texcolor.rgb * color.rgb;
             float3 speculer = pow(saturate(dot(reflect, eyedir)), dirLights[i].shininess) * m_specular;
-            float3 color = saturate((diffuse + speculer) * dirLights[i].lightcolor);
+            float3 color = saturate((smoothstep(threshold.x, threshold.y, diffuse) + smoothstep(0.5f, 0.55f, speculer)) * dirLights[i].lightcolor);
+
             shadeColor.rgb += color.rgb;
 
         }
@@ -32,11 +34,11 @@ PSOutput main(GSOutput input)
             float d = length(lightVec);
             lightVec = normalize(lightVec);
             float atten = 1.0f / (pLights[j].lightAtten.x + pLights[j].lightAtten.y * d + pLights[j].lightAtten.z * d * d);
-            float3 dotLightNormal = saturate(dot(lightVec, input.normal));
+            float3 dotLightNormal = dot(lightVec, input.normal);
             float3 reflect = normalize(-lightVec + 2 * dotLightNormal * input.normal);
-            float3 diffuse = dotLightNormal * m_diffuse * texcolor.rgb * color.rgb;
+            float3 diffuse = saturate(dotLightNormal) * m_diffuse * texcolor.rgb * color.rgb;
             float3 specular = pow(saturate(dot(reflect, eyedir)), pLights[j].shininess) * m_specular;
-            float3 color = (saturate(diffuse + specular) * pLights[j].lightColor);
+            float3 color = saturate((smoothstep(threshold.x, threshold.y, diffuse) + smoothstep(0.5f, 0.55f, specular)) * dirLights[j].lightcolor);
             shadeColor.rgb += color.rgb;
         }
     }
@@ -52,17 +54,162 @@ PSOutput main(GSOutput input)
             float cos = dot(lightVec, sLights[k].ligntVec);
             float angleAtten = smoothstep(sLights[k].lightFactorAngleCos.y, sLights[k].lightFactorAngleCos.x, cos);
             atten *= angleAtten;
-            float3 dotLightNormal = saturate(dot(lightVec, input.normal));
+            float3 dotLightNormal = dot(lightVec, input.normal);
             float3 reflect = normalize(-lightVec + 2 * dotLightNormal * input.normal);
-            float3 diffuse = dotLightNormal * m_diffuse * texcolor.rgb * color.rgb;
+            float3 diffuse = saturate(dotLightNormal) * m_diffuse * texcolor.rgb * color.rgb;;
+            float3 specular = pow(saturate(dot(reflect, eyedir)), sLights[k].shininess) * m_specular;
+            float3 color = saturate((smoothstep(threshold.x, threshold.y, diffuse) + smoothstep(0.5f, 0.55f, specular)) * dirLights[k].lightcolor);
+            shadeColor.rgb += color.rgb;
+
+        }
+    }
+	
+    output.target0 = shadeColor;
+    //output.target1 = float4(1 - ((shadeColor * texcolor) * color).rgb, 1);
+    output.target1 = output.target0;
+    return output;
+}
+
+PSOutput PhoneShader(GSOutput input)
+{
+    PSOutput output;
+    float4 texcolor = float4(tex.Sample(smp, input.uv));
+    float3 eyedir = normalize(cameraPos - input.worldpos.xyz);
+    float3 ambient = m_ambient;
+    float4 shadeColor = float4(ambientColor * ambient, m_alpha) * texcolor * color;
+    for (int i = 0; i < DIRLIGHT_NUM; i++)
+    {
+        if (dirLights[i].active)
+        {
+            float3 dotlightnormal = dot(dirLights[i].lightv, input.normal);
+            float3 reflect = normalize(-dirLights[i].lightv + 2 * dotlightnormal * input.normal);
+            float3 diffuse = saturate(dotlightnormal) * m_diffuse * texcolor.rgb * color.rgb;
+            float3 speculer = pow(saturate(dot(reflect, eyedir)), dirLights[i].shininess) * m_specular;
+            float3 color = saturate((diffuse + speculer) * dirLights[i].lightcolor);
+
+            shadeColor.rgb += color.rgb;
+
+        }
+    }
+
+    for (int j = 0; j < PLIGHT_NUM; j++)
+    {
+        if (pLights[j].active)
+        {
+            float3 lightVec = pLights[j].lightPos - input.worldpos.xyz;
+            float d = length(lightVec);
+            lightVec = normalize(lightVec);
+            float atten = 1.0f / (pLights[j].lightAtten.x + pLights[j].lightAtten.y * d + pLights[j].lightAtten.z * d * d);
+            float3 dotLightNormal = dot(lightVec, input.normal);
+            float3 reflect = normalize(-lightVec + 2 * dotLightNormal * input.normal);
+            float3 diffuse = saturate(dotLightNormal) * m_diffuse * texcolor.rgb * color.rgb;
+            float3 specular = pow(saturate(dot(reflect, eyedir)), pLights[j].shininess) * m_specular;
+            float3 color = saturate((diffuse + specular) * pLights[j].lightColor);
+            shadeColor.rgb += color.rgb;
+        }
+    }
+	
+    for (int k = 0; k < SLIGHT_NUM; k++)
+    {
+        if (sLights[k].active)
+        {
+            float3 lightVec = sLights[k].lightPos - input.worldpos.xyz;
+            float d = length(lightVec);
+            lightVec = normalize(lightVec);
+            float atten = saturate(1.0f / (sLights[k].lightAtten.x + sLights[k].lightAtten.y * d + sLights[k].lightAtten.z * d * d));
+            float cos = dot(lightVec, sLights[k].ligntVec);
+            float angleAtten = smoothstep(sLights[k].lightFactorAngleCos.y, sLights[k].lightFactorAngleCos.x, cos);
+            atten *= angleAtten;
+            float3 dotLightNormal = dot(lightVec, input.normal);
+            float3 reflect = normalize(-lightVec + 2 * dotLightNormal * input.normal);
+            float3 diffuse = saturate(dotLightNormal) * m_diffuse * texcolor.rgb * color.rgb;;
             float3 specular = pow(saturate(dot(reflect, eyedir)), sLights[k].shininess) * m_specular;
             float3 color = saturate((diffuse + specular) * sLights[k].lightColor);
             shadeColor.rgb += color.rgb;
 
         }
     }
+	
     output.target0 = shadeColor;
+    //output.target1 = float4(1 - ((shadeColor * texcolor) * color).rgb, 1);
     output.target1 = output.target0;
-    
     return output;
 }
+
+
+PSOutput rimLight(GSOutput input)
+{
+    PSOutput output;
+    float3 rimColor = (0.05, 0.25, 0.25);
+    float4 texcolor = float4(tex.Sample(smp, input.uv));
+    float3 eyedir = normalize(cameraPos - input.worldpos.xyz);
+    float3 ambient = m_ambient;
+    float4 rimLight = (1 - pow(saturate(dot(input.normal, eyedir)), 1));
+    float4 shadeColor = float4(ambientColor * ambient, m_alpha) * texcolor * color;
+    for (int i = 0; i < DIRLIGHT_NUM; i++)
+    {
+        if (dirLights[i].active)
+        {
+            float3 dotlightnormal = dot(dirLights[i].lightv, input.normal);
+            float3 reflect = normalize(-dirLights[i].lightv + 2 * dotlightnormal * input.normal);
+            float3 diffuse = saturate(dotlightnormal) * m_diffuse * texcolor.rgb * color.rgb;
+            float3 speculer = pow(saturate(dot(reflect, eyedir)), dirLights[i].shininess) * m_specular;
+            float3 color = saturate((diffuse + speculer) * dirLights[i].lightcolor);
+
+            shadeColor.rgb += (1 - rimLight).rgb * color.rgb + rimLight.rgb * rimColor;
+
+        }
+    }
+
+    for (int j = 0; j < PLIGHT_NUM; j++)
+    {
+        if (pLights[j].active)
+        {
+            float3 lightVec = pLights[j].lightPos - input.worldpos.xyz;
+            float d = length(lightVec);
+            lightVec = normalize(lightVec);
+            float atten = 1.0f / (pLights[j].lightAtten.x + pLights[j].lightAtten.y * d + pLights[j].lightAtten.z * d * d);
+            float3 dotLightNormal = dot(lightVec, input.normal);
+            float3 reflect = normalize(-lightVec + 2 * dotLightNormal * input.normal);
+            float3 diffuse = saturate(dotLightNormal) * m_diffuse * texcolor.rgb * color.rgb;
+            float3 specular = pow(saturate(dot(reflect, eyedir)), pLights[j].shininess) * m_specular;
+            float3 color = saturate((diffuse + specular) * pLights[j].lightColor);
+            shadeColor.rgb += (1 - rimLight).rgb * color.rgb * rimLight.rgb * rimColor;
+        }
+    }
+	
+    for (int k = 0; k < SLIGHT_NUM; k++)
+    {
+        if (sLights[k].active)
+        {
+            float3 lightVec = sLights[k].lightPos - input.worldpos.xyz;
+            float d = length(lightVec);
+            lightVec = normalize(lightVec);
+            float atten = saturate(1.0f / (sLights[k].lightAtten.x + sLights[k].lightAtten.y * d + sLights[k].lightAtten.z * d * d));
+            float cos = dot(lightVec, sLights[k].ligntVec);
+            float angleAtten = smoothstep(sLights[k].lightFactorAngleCos.y, sLights[k].lightFactorAngleCos.x, cos);
+            atten *= angleAtten;
+            float3 dotLightNormal = dot(lightVec, input.normal);
+            float3 reflect = normalize(-lightVec + 2 * dotLightNormal * input.normal);
+            float3 diffuse = saturate(dotLightNormal) * m_diffuse * texcolor.rgb * color.rgb;;
+            float3 specular = pow(saturate(dot(reflect, eyedir)), sLights[k].shininess) * m_specular;
+            float3 color = saturate((diffuse + specular) * sLights[k].lightColor);
+            shadeColor.rgb += (1 - rimLight).rgb * color.rgb * rimLight.rgb * rimColor;
+
+        }
+    }
+	
+    output.target0 = shadeColor;
+    //output.target1 = float4(1 - ((shadeColor * texcolor) * color).rgb, 1);
+    output.target1 = output.target0;
+    return output;
+}
+
+PSOutput main(GSOutput input)
+{
+    PSOutput output;
+    output = toonShader(input);
+    return output;
+}
+
+
--- DirectX12CG/Resources/Shaders/PostEffectShader/postEffectPixelShader.hlsl
+++ DirectX12CG/Resources/Shaders/PostEffectShader/postEffectPixelShader.hlsl
@@ -4,28 +4,106 @@ Texture2D<float4> tex0 : register(t0);
 Texture2D<float4> tex1 : register(t1);
 SamplerState smp : register(s0);
 
+float4 AverageBlur(VSOutput input, Texture2D<float4> tex)
+{
+    float4 texColor1 = float4(tex.Sample(smp, input.uv));
+    const float uChange = 1 / 1280.f;
+    const float vChange = 1 / 720.f;
+    float4 texColor1sum = { 0, 0, 0, 0 };
+    const float uForRange = 6;
+    const float vForRange = 6;
+    for (float v = 0; v < vForRange; v++)
+    {
+        for (float u = 0; u < uForRange; u++)
+        {
+            float2 offSet = float2((u - 1) * uChange, (v - 1) * vChange);
+            texColor1sum += tex.Sample(smp, saturate(input.uv - offSet));
+        }
+    }
+    return texColor1sum / (uForRange * vForRange);
+}
+
+float4 inv(VSOutput input, Texture2D<float4> tex)
+{
+    return float4(1 - tex.Sample(smp, input.uv).rgb, 1);
+}
+
+float4 gray(VSOutput input,Texture2D<float4> tex)
+{
+    float4 color = float4(tex.Sample(smp, input.uv));
+    float l = (color.r + color.g + color.b) / 3;
+    return float4(l, l, l, 1);
+
+}
+
+float4 sepia(VSOutput input,Texture2D<float4> tex,float sepiaColor)
+{
+    float4 color = float4(tex.Sample(smp, input.uv));
+    float l = (color.r + color.g + color.b) / 3;
+    return float4(l + sepiaColor, l, saturate(l - sepiaColor), 1);
+}
+
+float Gaussian(float2 drawUV, float2 piclUV, float sigma)
+{
+    float d = distance(drawUV, piclUV);
+    return exp(-(d * d) / (2 * sigma * sigma));
+}
+
+float4 GaussianBlur(VSOutput input, Texture2D<float4> tex)
+{
+    float totalWeght = 0, _Sigma = 0.005f, _StepWight = 0.001f;
+    float4 color = float4(0, 0, 0, 0);
+    float forRange = _Sigma * 2;
+    for (float py = -forRange; py <= forRange; py += _StepWight)
+    {
+        for (float px = -forRange; px <= forRange; px += _StepWight)
+        {
+            float2 pickUV = input.uv + float2(px, py);
+            float weght = Gaussian(input.uv, pickUV, _Sigma);
+            color += tex.Sample(smp, pickUV) * weght;
+            totalWeght += weght;
+        }
+    }
+    color.rgb = color.rgb / totalWeght;
+    return color;
+
+}
+
 float4 main(VSOutput input) : SV_TARGET
 {
-    float4 texColor1 = float4(tex1.Sample(smp, input.uv));
-    if (fmod(input.uv.y, 0.1f) < 0.05f)
+    switch (color.r)
     {
-        float uChange = 1 / 1280.f;
-        float vChange = 1 / 720.f;
-        float4 texColor1sum = { 0, 0, 0, 0 };
-        float uForRange = 6;
-        float vForRange = 6;
-        for (float v = 0; v < vForRange; v++)
-        {
-            for (float u = 0; u < uForRange; u++)
+        case 1:
+            //if (fmod(input.uv.y, 0.1f) < 0.05f)
+            //{
+            //    return AverageBlur(input, tex0);
+            //}
+            //return inv(input, tex1);
+            return sepia(input, tex0, 0.024f);
+            break;
+        case 2:
+            return AverageBlur(input, tex0);
+            break;
+        case 3:
+            return inv(input, tex0);
+            break;
+        case 4:
+            return GaussianBlur(input, tex0);
+            break;
+        case 5:
+            return gray(input, tex0);
+            break;
+        case 6:
+            return sepia(input, tex0,0.024f);
+            break;
+        default:
+            if (fmod(input.uv.y, 0.1f) < 0.05f)
             {
-                float2 offSet = ((u - 1) * uChange, (v - 1) * vChange);
-                texColor1sum += tex1.Sample(smp, saturate(input.uv - offSet));
+                return AverageBlur(input,tex0); 
             }
-        }
-        return texColor1; //texColor1sum / (uForRange * vForRange);
-
+            return inv(input, tex1);
+            break;
     }
-    float4 texColor0 = float4(1 - tex0.Sample(smp, input.uv).rgb, 1);
-
-    return texColor1;
+    return float4(tex1.Sample(smp, input.uv));
+    
 }
